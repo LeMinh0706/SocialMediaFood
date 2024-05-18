1525a52b191730c394d6b991f22851de786d3e06
Minh Hieu <mhieusz.23.dkc@gmail.com>
2024-05-18 15:12:19 +0700

    format code

--- src/Services/AxiosPost.jsx
+++ src/Services/AxiosPost.jsx
@@ -20,20 +20,21 @@ import axios from "./AxiosCustomize";
 // export { createPost }
 
 const createPost = async (userId, accessToken, description, image) => {
-    const formData = new FormData();
-    formData.append('description', description);
-    formData.append('image', image);
+  const formData = new FormData();
+  formData.append("description", description);
+  formData.append("image", image);
 
-    try {
-        const response = await axios.post(`post/create-post/${userId}`, formData, {
-            headers: {
-                'Authorization': `Bearer ${accessToken}`,
-                'Content-Type': 'multipart/form-data',
-            },
-        });
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const response = await axios.post(`post/create-post/${userId}`, formData, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
-export { createPost }
+export { createPost };
