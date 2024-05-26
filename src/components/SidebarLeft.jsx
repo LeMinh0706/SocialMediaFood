69ec0dd45290bf937e651b780e2e688bf52b18a3
Người Làm Tình Màn Ảnh <153808638+LeMinh0706@users.noreply.github.com>
2024-05-26 19:54:10 +0700

    update logout

--- src/components/SidebarLeft.jsx
+++ src/components/SidebarLeft.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { userLogout } from '../action/useAction';
+import { useNavigate } from 'react-router-dom';
 
 const SidebarLeft = () => {
 
     const isAuthenticated = useSelector(state => state.user.isAuthenticated)
+
+    const navigate = useNavigate()
 
     const dispatch = useDispatch();
 
@@ -12,9 +15,8 @@ const SidebarLeft = () => {
         // Dispatch the logout action
         dispatch(userLogout());
         // Optional: Redirect to login page or home page
-        window.location.href = '/';
+        navigate('/');
     };
-
 
     return (
         <div className='flex flex-col justify-between fixed left-16 top-44 rounded-xl h-[70%] w-[12rem] bg-white border-2 shadow-lg text-gray-400 p-4'>
