e3cbda093aba6b12f435ecf66a8e16c549eaac4c
Minh Hieu <mhieusz.23.dkc@gmail.com>
2024-05-30 00:49:50 +0700

    update

--- src/pages/admin/components/HeaderAdmin.jsx
+++ src/pages/admin/components/HeaderAdmin.jsx
@@ -6,7 +6,10 @@ const HeaderAdmin = () => {
   return (
     <div className="bg-gray-900 text-white p-4 flex justify-between items-center">
       <h1 className="text-xl">Admin Dashboard</h1>
+     <div>
+     <button onClick= { () => navigate('/') } className="bg-red-600 hover:bg-red-700 text-white mr-5 py-2 px-4 rounded">Feed</button>
       <button onClick= { () => navigate('/login') } className="bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded">Logout</button>
+     </div>
     </div>
   );
 };
